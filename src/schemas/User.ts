d72db4ad258ad671cd1c1eb627adb120cfc39e15
Bruno Souza <brunoprogramadorjava@gmail.com>
2019-08-20 09:23:46 -0300

    [t01] Node environment with typescript

---
+++ src/schemas/User.ts
@@ -0,0 +1,21 @@
+import { Schema, Model, Document, model } from 'mongoose'
+
+import { UserInterface } from '../interfaces/User'
+
+export interface UserModel extends UserInterface, Document {
+    fullName(): string
+}
+
+const UserSchema = new Schema({
+  email: String,
+  firstName: String,
+  lastName: String
+}, {
+  timestamps: true
+})
+
+UserSchema.methods.fullName = function (): string {
+  return (this.firstName.trim() + ' ' + this.lastName.trim())
+}
+
+export const User: Model<UserModel> = model<UserModel>('User', UserSchema)
